e944c0d82a0be7dc6bf86eb993a447d8d1e19a76
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-05-29 14:38:57 +0200

    New exercice

---
+++ categorizeMembers.js
@@ -0,0 +1,11 @@
+function openOrSenior(data){
+  let result = [];
+  for(let i=0; i<data.length; i++){
+     if(data[i][0] > 54 && data[i][1] > 7){
+      result[i] = "Senior";
+     }else{
+      result[i] = "Open";
+     }
+  }
+  return result;
+}
