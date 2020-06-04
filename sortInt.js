78d37a43777be76fa6921f17050d9380635cf3f9
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-06-04 15:17:06 +0200

    New Ex

---
+++ sortInt.js
@@ -0,0 +1,9 @@
+function descendingOrder(n){
+  let num = Array.from(String(n), String);
+  num.sort(function(a, b){return b - a});
+  let res = num[0];
+  for(let i=1; i<num.length; i++){
+    res = res + num[i];
+  }
+  return parseInt(res, 10)
+}
