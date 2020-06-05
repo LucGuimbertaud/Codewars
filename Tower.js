62b9a22dedf180cd19eff15e02d9fdbb2e9a50cf
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-06-05 19:16:33 +0200

    Add Ex

---
+++ Tower.js
@@ -0,0 +1,14 @@
+function towerBuilder(nFloors) {
+  let res = [];
+  let star = "*";
+  let space = " ";
+  let count = 1;
+  let spaceCount = 1;
+
+  for(let i = 0; i<nFloors; i++){
+    res[i] = space.repeat(nFloors-spaceCount) + star.repeat(count) + space.repeat(nFloors-spaceCount);
+    count = count + 2;
+    spaceCount++;
+  }
+  return res;
+}
