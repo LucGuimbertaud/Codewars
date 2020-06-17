3743cac07c6fd1807e22028c834c3d06ebdd9974
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-06-17 20:27:56 +0200

    Add ex

---
+++ Eureka.js
@@ -0,0 +1,13 @@
+function sumDigPow(a, b) {
+ eureka = [];
+ for(i=a; i <=b; i++){
+   digits = String(i).split('');
+   if(i == digits.reduce(function(accumulator, currentValue, currentIndex){
+     return accumulator + currentValue**(currentIndex+1);
+   }, 0)){
+     eureka.push(i);
+   }
+ }
+ return eureka;
+}
+
