9c85f1523afc8e321077ec68c6760b26aa919e98
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-07-02 23:58:22 +0200

    Change Ex

--- Tower.js
+++ Tower.js
@@ -4,7 +4,6 @@ function towerBuilder(nFloors) {
   let space = " ";
   let count = 1;
   let spaceCount = 1;
-
   for(let i = 0; i<nFloors; i++){
     res[i] = space.repeat(nFloors-spaceCount) + star.repeat(count) + space.repeat(nFloors-spaceCount);
     count = count + 2;
