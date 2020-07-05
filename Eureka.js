5879014eec5961709115f81c131831036bc879f9
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-07-05 21:06:58 +0200

    Minor change

--- Eureka.js
+++ Eureka.js
@@ -1,6 +1,6 @@
 function sumDigPow(a, b) {
  eureka = [];
- for(i=a; i <=b; i++){
+ for(let i=a; i <=b; i++){
    digits = String(i).split('');
    if(i == digits.reduce(function(accumulator, currentValue, currentIndex){
      return accumulator + currentValue**(currentIndex+1);
