bdfd3840cbceaaccc2aa201ca6410c13888e2e75
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-07-08 22:51:43 +0200

    Add Ex

---
+++ UniqueInOrder.js
@@ -0,0 +1,9 @@
+var uniqueInOrder=function(iterable){
+  let sol = [];
+  for(let i=0; i<iterable.length; i++){
+    if(iterable[i] != iterable[i-1]){
+      sol.push(iterable[i])
+    }
+  }
+  return sol;
+}
