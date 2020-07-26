bf7a2c1f5e1b040b10eb80df7a6948480a9a8037
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-07-26 14:45:04 +0200

    new ex

--- UniqueInOrder.js
+++ UniqueInOrder.js
@@ -1,5 +1,5 @@
 var uniqueInOrder=function(iterable){
-  let sol = [];
+   let sol = [];
   for(let i=0; i < iterable.length; i++){
     if(iterable[i] != iterable[i-1]){
       sol.push(iterable[i])
